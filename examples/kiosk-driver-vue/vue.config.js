ae216fd452dea380a7c6274a2ac5b4191162c5c0
Jon Cron <jonnmb@gmail.com>
2019-01-28 14:29:31 -0700

    Vue example is now driven by environment variables for builds

--- examples/kiosk-driver-vue/vue.config.js
+++ examples/kiosk-driver-vue/vue.config.js
@@ -1,11 +1,10 @@
 const RemoveStrictPlugin = require('remove-strict-webpack-plugin');
-
 // Webpack plugin to strip out "strict mode" from JS files that was preventing this
 //  from running on Chrome/Chromium 27-29
 module.exports = {
-  configureWebpack: {
-		plugins: [
-			new RemoveStrictPlugin()
-		]
+  chainWebpack: config => {
+		if ( process.env.BUILD === 'legacy' ){
+			config.plugin('RemoveStrictPlugin').use(RemoveStrictPlugin).end();
+		}
 	}
 }
